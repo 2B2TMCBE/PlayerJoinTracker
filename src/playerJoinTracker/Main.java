9d8f4aa23929cdff990c28bfe3c9b27d3ac21aca
maxxie114 <maxxie114@gmail.com>
2018-10-26 14:50:16 -0700

    Add feature and commands to return the last join time of a player.
    1. Command is /lastjoin
    2. Added javadocs, and necessary comments.
    3. Add chat formats.

---
+++ src/playerJoinTracker/Main.java
@@ -0,0 +1,46 @@
+package playerJoinTracker;
+
+import java.time.LocalDateTime;
+// import from java library
+import java.util.HashMap;
+
+// import from Nukkitx
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
+
+public class Main extends PluginBase {
+	
+	// create private variables
+	// @SuppressWarnings("unused")
+	// private EventListener listener;
+	
+	// Create public variables
+	public HashMap<String, LocalDateTime> joindata = new HashMap<>(); 
+	
+	@Override
+	public void onLoad() {
+	  this.getLogger().info(TextFormat.YELLOW + "PlayerJoinTracker loaded");
+	}
+	@Override
+	public void onEnable() {
+	  this.getLogger().info(TextFormat.GREEN + "PlayerJoinTracker enabled.");
+	  this.getServer().getPluginManager().registerEvents(new EventListener(this),this);
+	}
+	@Override
+	public void onDisable() {
+	  this.getLogger().info(TextFormat.RED + "PlayerJoinTracker disabled.");
+	}
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    	if(command.getName().equalsIgnoreCase("lastjoin")) {
+    		if(joindata.containsKey(args[0])) {
+    		  LocalDateTime time = joindata.get(args[0]);
+    		  sender.sendMessage(TextFormat.BLUE + "Player, " + args[0] + " " + "last joined in " + time.toString());  
+    		}
+    	}
+        return true;
+    }
+	
+}
