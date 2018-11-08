87c115d5de0f978b45bcb15d4233ff6048094561
maxxie114 <maxxie114@gmail.com>
2018-11-08 15:34:24 -0800

    Convert Project to Maven
    1. Fix up some Coding Style Errors
    2. Remove useless code
    3. Convert Project to Maven

--- src/main/java/Main/Main/Main/Main.java
+++ src/main/java/Main/Main/Main/Main.java
@@ -1,12 +1,10 @@
-package Main.Main.Main;
+package main.java.Main.Main.Main;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-import Events.EventListener;
 import cn.nukkit.Player;
-import cn.nukkit.Server;
 // import from Nukkitx
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
@@ -14,7 +12,7 @@ import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 
 public class Main extends PluginBase {
-	
+
   // create private variables
   // @SuppressWarnings("unused")
   // private EventListener listener;
@@ -23,41 +21,54 @@ public class Main extends PluginBase {
   public void onLoad() {
     this.getLogger().info(TextFormat.YELLOW + "PlayerJoinTracker loaded");
   }
+
   @Override
   public void onEnable() {
-    regEvents();
+    // regEvents();
     this.getLogger().info(TextFormat.GREEN + "PlayerJoinTracker enabled.");
   }
+
   @Override
   public void onDisable() {
     this.getLogger().info(TextFormat.RED + "PlayerJoinTracker disabled.");
   }
-  public void regEvents() {
-    //this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
-  }
+
+  /*
+   * public void regEvents() { //this.getServer().getPluginManager().registerEvents(new
+   * EventListener(this), this); }
+   */
+
   @Override
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    if (!(sender instanceof Player)){
+    if (!(sender instanceof Player)) {
       sender.sendMessage("Only Players Can Use This Command.");
       return true;
-  }
-    Player p = (Player) sender;
-    Player Target = Server.getInstance().getPlayer(args[0]);
-    
-    if(cmd.getName().equalsIgnoreCase("lastjoin")) {
-      if (args.length == 1) {
-        p.sendMessage(TextFormat.BLUE + "Player, " + args[0] + " " + "last joined in " + convertTime(Target));
+    }
+    // Player p = (Player) sender;
+    Player target = this.getServer().getPlayer(args[0]);
+    if (cmd.getName().equalsIgnoreCase("lastjoin")) {
+      if (args.length == 1 && target != null) {
+        sender.sendMessage(
+            TextFormat.BLUE + "Player, " + args[0] + " " + "last joined in " + convertTime(target));
         return true;
       } else {
-        p.sendMessage(TextFormat.RED + "Player doesn't exist, please try again.");
+        sender.sendMessage(TextFormat.RED + "Player doesn't exist, please try again.");
         return true;
       }
     }
     return true;
   }
-  public String convertTime(Player Target){
-    Date date = new Date(TimeUnit.SECONDS.toMillis(Target.getLastPlayed()));
+
+  public String convertTime(Player target) {
+    Date date = new Date(TimeUnit.SECONDS.toMillis(target.getLastPlayed()));
     Format format = new SimpleDateFormat("YYYY-MM-dd-HH:mm:ss");
     return format.format(date).toString();
-}
+  }
+
+  /** just for test */
+  public String test(String input) {
+    return input;
+
+  }
+
 }
